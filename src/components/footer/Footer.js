3fbb6a0154b4450c156f1e9a5529a2dd0269d39a
fadli <fadlimuharram@hotmail.com>
2019-04-02 09:32:23 +0700

    fix rel

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,18 @@ import React from "react";
 
 const Footer = () => (
   <footer class="footer" id="contact">
-    <a href="https://github.com/fadlimuharram" target="_blank">
+    <a
+      href="https://github.com/fadlimuharram"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
       <i class="socmend--icon icon-github">&#xf300;</i>
     </a>
     <i class="socmend--icon icon-facebook">&#xf30c;</i>
     <i class="socmend--icon icon-twitter">&#xf315;</i>
     <a
       href="https://www.linkedin.com/in/fadli-muharram-402204113/"
+      rel="noopener noreferrer"
       target="_blank"
     >
       <i class="socmend--icon icon-linkedin">&#xf318;</i>
